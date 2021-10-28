b2d0bc25f5f3ba3dc6f1aec8989071e3925e438b
zephyrus21 <pandey.piyus@gmail.com>
2021-10-28 21:50:37 +0530

    bubble sort

---
+++ Sorting/bubble.cpp
@@ -0,0 +1,45 @@
+/* 
+!-----BUBBLE SORT-----!
+
+$ Bubble Sort Algorithim implementation in Array.
+
+* Time Complexity:
+# Best Case: O(n)
+# Average Case: O(n^2)
+# Worst Case: O(n^2)
+
+* Space Complexity: O(1)
+
+@author [Piyush Pandey](https://github.com/zephyrus21)
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void bubble_sort(int arr[], int n) {
+  bool flag;
+  for (int i = 0; i < n - 1; i++) {
+    flag = false; //? flag is used to check if any swap is done in this iteration
+    for (int j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) { //? swap the elements if the current element is greater than the next element
+        int temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        flag = true;
+      }
+    }
+    if (!flag)
+      return;
+  }
+}
+
+int main() {
+  int arr[] = {64, 25, 92, 23, 11};
+  bubble_sort(arr, 5);
+
+  for (int i = 0; i < 5; i++)
+    cout << arr[i] << " ";
+
+  return 0;
+}
--- Sorting/selection.cpp
+++ Sorting/selection.cpp
@@ -18,15 +18,11 @@ $ Selection Sort Algorithim implementation in Array.
 using namespace std;
 
 void selection_sort(int arr[], int n) {
-  for (int i = 0; i < n - 1; i++) { //? this will run n-1 times
-
+  for (int i = 0; i < n - 1; i++) {
     int min_idx = i; //? this will store the index of minimum element
-
-    for (int j = i + 1; j < n; j++) { //? this will run from i+1 pos to n
-
+    for (int j = i + 1; j < n; j++)
       if (arr[j] < arr[min_idx]) //? if current element is less than the minimum element then update the minimum element index
         min_idx = j;
-    }
     int temp = arr[i];
     arr[i] = arr[min_idx];
     arr[min_idx] = temp;
@@ -37,8 +33,8 @@ int main() {
   int arr[] = {64, 25, 12, 22, 11};
   selection_sort(arr, 5);
 
-  for (int i = 0; i < 5; i++) {
+  for (int i = 0; i < 5; i++)
     cout << arr[i] << " ";
-  }
+
   return 0;
 }
