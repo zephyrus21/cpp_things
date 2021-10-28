3caf0b98627ffe4ad9cda277ff38fecfaf9b1514
zephyrus21 <pandey.piyus@gmail.com>
2021-10-28 17:37:21 +0530

    selection sort

---
+++ Sorting/selection.cpp
@@ -0,0 +1,40 @@
+/* 
+!-----SELECTION SORT-----!
+
+* Time Complexity:
+# Best Case: O(n^2)
+# Average Case: O(n^2)
+# Worst Case: O(n^2)
+
+*Space Complexity: O(1)
+
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void selection_sort(int arr[], int n) {
+  for (int i = 0; i < n - 1; i++) { //? this will run n-1 times
+
+    int min_idx = i; //? this will store the index of minimum element
+
+    for (int j = i + 1; j < n; j++) { //? this will run from i+1 pos to n
+
+      if (arr[j] < arr[min_idx]) //? if current element is less than the minimum element then update the minimum element index
+        min_idx = j;
+    }
+    int temp = arr[i];
+    arr[i] = arr[min_idx];
+    arr[min_idx] = temp;
+  }
+}
+
+int main() {
+  int arr[] = {64, 25, 12, 22, 11};
+  selection_sort(arr, 5);
+
+  for (int i = 0; i < 5; i++) {
+    cout << arr[i] << " ";
+  }
+}
